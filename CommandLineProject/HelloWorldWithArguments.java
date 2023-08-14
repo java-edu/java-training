3d52e2e62c053345ec5678c7c7559482fe249bcb
Java Training <140969715+java-edu@users.noreply.github.com>
2023-08-14 05:57:33 +0530

    august 13 cst

---
+++ CommandLineProject/HelloWorldWithArguments.java
@@ -0,0 +1,7 @@
+
+public class HelloWorldWithArguments {
+
+	public static void main(String[] args){
+		System.out.println("Hello " + args[0]);
+	}
+}
---
+++ HelloWorld/src/com/aug/sessions/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.aug.sessions;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
